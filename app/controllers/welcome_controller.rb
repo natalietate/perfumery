a400a685d23ce293b6b482b6b4737831114d87bc
natalietate <martin.nat@gmail.com>
2018-01-22 11:44:06 -0500

    links to perfume show pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,5 @@ class WelcomeController < ApplicationController
   def index
     @perfumes = Perfume.all
     @brands = Brand.all
-    @brand = Brand.find_by_id(params[:id])
-    @perfume = Perfume.find_by_id(params[:id])
-
   end
 end
